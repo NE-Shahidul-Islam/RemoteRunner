7cc0e992e5a7c75c964cb5e67853e0e3679fabb2
Shahidul Islam <shahidislamcse123@gmail.com>
2024-05-02 02:23:40 +0600

    First Project

---
+++ src/ProductWithVarriable.java
@@ -0,0 +1,11 @@
+public class ProductWithVarriable {
+    public static void main(String[] args) {
+        int id=30;
+        float price=102090.560f;
+        System.out.println("Id="+id);
+        System.out.printf("Price=%f tk\n",price);
+        String name="I Phone";
+        System.out.println("Name="+name);
+
+    }
+}
